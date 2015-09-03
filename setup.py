c2fcc343c764c8b81caf7f51f2b4a98e79038791
Klaas Millet <klaas.millet@rwo.vlaanderen.be>
2015-09-03 14:29:19 +0200

    CHANGES updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = ['requests']
 
 setup(
     name='sphinx-vlaamsecodex',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/OnroerendErfgoed/sphinx-vlaamsecodex',
     license='MIT',
     author='Flanders Heritage Agency',
